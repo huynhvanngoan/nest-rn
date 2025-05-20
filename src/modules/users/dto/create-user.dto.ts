b026ee207e5f5ebc617dcd6bfece5a71205c2409
Huynh Van Ngoan <huynhngoan389@gmail.com>
2025-05-20 09:19:15 +0700

    add crud user,auth/login, create access token

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name is not empty' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Email is not empty' })
+  @IsEmail({}, { message: 'Email is not valid' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is not empty' })
+  // @IsStrongPassword({
+  //   minLength: 8,
+  //   minLowercase: 1,
+  //   minUppercase: 1,
+  //   minNumbers: 1,
+  //   minSymbols: 1,
+  // })
+  password: string;
+
+  // @IsNotEmpty({ message: 'Phone is not empty' })
+  // @IsPhoneNumber('VN', { message: 'Phone is not valid' })
+  phone: string;
+
+  // @IsNotEmpty({ message: 'Address is not empty' })
+  address: string;
+
+  image: string;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { hashPasswordHelper } from '@/helpers/utils';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import mongoose, { Model } from 'mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  isEmailExist = async (email: string) => {
+    const userExist = await this.userModel.exists({ email });
+    if (userExist) return true;
+    return false;
+  };
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password, phone, address, image } = createUserDto;
+
+    // check email is exist
+    const isExist = await this.isEmailExist(email);
+    if (isExist) {
+      throw new BadRequestException(
+        `Email is already exist: ${email}. Please use another email`,
+      );
+    }
+
+    const hashPassword = await hashPasswordHelper(password);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashPassword,
+      phone,
+      address,
+      image,
+    });
+
+    return {
+      _id: user._id,
+    };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(query: string, current: number, pageSize: number) {
+    const { filter, sort } = aqp(query);
+    if (filter.current) delete filter.current;
+    if (filter.pageSize) delete filter.pageSize;
+    if (!current) current = 1;
+    if (!pageSize) pageSize = 10;
+
+    const totalItems = (await this.userModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+    const skip = (current - 1) * pageSize;
+
+    const result = await this.userModel
+      .find(filter)
+      .limit(pageSize)
+      .select('-password')
+      .skip(skip)
+      .sort(sort as any);
+    return {
+      result,
+      totalItems,
+      totalPages,
+    };
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findByEmail(email: string) {
+    return await this.userModel.findOne({ email });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne(
+      { _id: updateUserDto._id },
+      { ...updateUserDto },
+    );
+  }
+
+  async remove(_id: string) {
+    // check id is valid
+    if (mongoose.isValidObjectId(_id)) {
+      return this.userModel.deleteOne({ _id });
+    } else {
+      throw new BadRequestException('Invalid id');
+    }
   }
 }
